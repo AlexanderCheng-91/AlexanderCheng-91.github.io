27671d32ee53f59366dbc12aa42297d3493c443f
Alexander Cheng <achengster4@gmail.com>
2021-04-19 15:13:29 -0700

    first commit to about-me3

---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {Navbar} from 'reactstrap';
+
+class NavBar extends React.Component{
+    render(){
+        return(
+            <Navbar>
+
+            </Navbar>
+        )
+    }
+
+}
+
+export default NavBar;
