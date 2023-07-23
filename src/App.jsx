9bb809d0cd110d6d8c8d77172bd7f4b0e65c9ed6
AlexanderCheng <achengster4@gmail.com>
2023-07-23 05:59:13 -0700

    commented out a bunch of code and testing about me component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css';
-// import React from "react"
+import React from 'react';
 
-import LandingPage from './components/LandingPage';
-import Navbar1 from './components/NavBar1';
+// comment out to test
+
+// import LandingPage from './components/LandingPage';
+// import Navbar1 from './components/NavBar1';
 import AboutMe from './components/AboutMe';
-// import AlexCard from './components/AlexCard';
+// import Blogs from './components/Blogs';
+// import BlogGracie from './components/BlogGracie';
+// import BlogDayInTheLife from './components/BlogDayInTheLife';
+// import BlogCareer from './components/BlogCareer';
+// import Footer from './components/Footer';
+
+//forgot why I commented this out
+//import AlexCard from './components/AlexCard';
 // import AlexGraph from './components/AlexGraph';
+
+// commented out the cauousel
 // import Projects from './components/Projects';
-import Blogs from './components/Blogs';
+
+// old commented out code
 // want to route to a "different page" for gracie blog
-import BlogGracie from './components/BlogGracie';
-import BlogDayInTheLife from './components/BlogDayInTheLife';
-import BlogCareer from './components/BlogCareer';
-import Footer from './components/Footer';
 
 import { Col, Container } from 'reactstrap';
 
@@ -23,31 +31,33 @@ function App() {
       <div className="App">
          <Container fluid style={{ padding: 0 }}>
             <Col style={{ padding: 0 }}>
-            
-               <Switch >
-                  <Route exact path="/gracie">
-                     <BlogGracie></BlogGracie>
-                  </Route>
-                  <Route exact path="/dayInTheLife">
-                     <BlogDayInTheLife></BlogDayInTheLife>
-                  </Route>
-                  <Route exact path="/career">
-                     <BlogCareer></BlogCareer>
-                  </Route>
-                  <Route exact path="/blogs">
-                     <Blogs></Blogs>
-                  </Route>
-                  <Route exact path="/">
-                     <LandingPage></LandingPage>
-                     <Navbar1></Navbar1>
-                     <AboutMe></AboutMe>
+               <AboutMe/>
+               {/* <Switch > */}
+                  {/* <Route exact path="/gracie"> */}
+                     {/* <BlogGracie></BlogGracie> */}
+                  {/* </Route> */}
+                  {/* <Route exact path="/dayInTheLife"> */}
+                     {/* <BlogDayInTheLife></BlogDayInTheLife> */}
+                  {/* </Route> */}
+                  {/* <Route exact path="/career"> */}
+                     {/* <BlogCareer></BlogCareer> */}
+                  {/* </Route> */}
+                  {/* <Route exact path="/blogs"> */}
+                     {/* <Blogs></Blogs> */}
+                  {/* </Route> */}
+                  {/* <Route exact path="/"> */}
+                     {/* <LandingPage></LandingPage> */}
+                     {/* <Navbar1></Navbar1> */}
+                     {/* <AboutMe></AboutMe> */}
+
                      {/* <AlexCard></AlexCard> */}
                      {/* <AlexGraph></AlexGraph> */}
                      {/* <Projects></Projects> */}
-                     <Blogs></Blogs>
-                     <Footer></Footer>
-                  </Route>
-               </Switch>
+                     
+                     {/* <Blogs></Blogs> */}
+                     {/* <Footer></Footer> */}
+                  {/* </Route> */}
+               {/* </Switch> */}
                
             </Col>
          </Container>
