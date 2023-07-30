07dca94dbc236adde1b4b1426ee95f679ff9e0f0
AlexanderCheng <achengster4@gmail.com>
2023-07-29 20:44:25 -0700

    changed back homepage to default and changed private to false

---
+++ src/components/ContactForm2.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+// import { Button, Form, FormGroup, Input } from 'reactstrap';
+// import Swal from 'sweetalert2';
+
+
+class ContactForm2 extends React.Component {
+    
+ 
+    render() {
+       return (
+        <div>
+            <form action="https://api.web3forms.com/submit" method="POST">
+                <input
+                type="hidden"
+                name="access_key"
+                defaultValue="0855412d-bdab-4d1c-8709-c215f8999a71"
+                />
+        <div>
+            
+                <input type="text" name="name" required="" defaultValue={"name"}/>
+        </div>
+        <div>
+            
+                <input type="email" name="email" required="" defaultValue={"email"} />
+        </div>
+            
+                <textarea name="message" required="" defaultValue={"your message here"} />
+        <div className="h-captcha" data-captcha="true" />
+            <button type="submit">Submit Form</button>
+      </form>
+      </div>
+       )
+    }
+}
+
+export default ContactForm2;
